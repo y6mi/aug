2eecb5fa76b4b9dd2b2d6a6a6dc13a2427d69a62
yami <81629507+y6mi@users.noreply.github.com>
2025-01-08 11:50:41 -0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// <!-- typed js effect starts -->
+// <!-- home page typed js effect starts -->
 var typed = new Typed(".typing-text", {
     strings: ["AWFUL", "UGLY", "GREEDY", "..."],
     loop: true,
@@ -21,18 +21,18 @@ var typed = new Typed(".typing-text", {
     backSpeed: 25,
     backDelay: 500,
 });
-// <!-- typed js effect ends -->
+// <!-- home page typed js effect ends -->
 
 // disable right clicking on the image
 document.querySelector('.logo img').addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
-
+/*
 // yt thumbnail event
 document.querySelector('.video-thumbnail').addEventListener('click', () => {
     window.open('https://www.youtube.com/watch?v=Ps0dIDMhUzg&ab_channel=AUGRecords', '_blank');
 });
-
+*/
 
 //menu icon click listener
 document.querySelector('.menu-icon').addEventListener('click', () => {
@@ -46,3 +46,5 @@ document.querySelector('.close-menu').addEventListener('click', () => {
     const popupMenu = document.querySelector('.popup-menu');
     popupMenu.style.display = 'none';
 });
+
+
